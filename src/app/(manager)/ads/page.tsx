947ca1f3b31530ecfc2159f90bcde43fd164536d
Chang Woo Park <whwh3560@gmail.com>
2023-10-18 00:00:47 +0900

    feat: [front] resize grid

--- src/app/(manager)/ads/page.tsx
+++ src/app/(manager)/ads/page.tsx
@@ -3,12 +3,14 @@ import { instance } from '@/utils/woxios';
 import { use } from 'react';
 
 export default function Page({ searchParams }: { searchParams: { type: string } }) {
-  const data: any = use(instance.get('/admin/select', { params: { type: searchParams.type }, cache: 'no-cache' }));
+  const { type = 'main' } = searchParams;
+
+  const data: any = use(instance.get('/admin/select', { params: { type }, cache: 'force-cache' }));
 
   return (
     <main className="min-h-screen bg-gray-50 flex justify-center">
       <section className="w-[1200px] h-[700px] py-30 px-10 overflow-x-scroll overflow-y-scroll">
-        <GridProvider data={data?.data?.data}></GridProvider>
+        <GridProvider data={data?.data?.data} />
       </section>
     </main>
   );
--- src/constants/column.tsx
+++ src/constants/column.tsx
@@ -1,5 +1,5 @@
 import { ColumnDef, createColumnHelper } from '@tanstack/react-table';
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 
 interface AD {
   id: string;
@@ -19,48 +19,53 @@ interface AD {
   edit: string;
 }
 
-const columnHelper = createColumnHelper();
+// const columnHelper = createColumnHelper();
 
-export const columns: any = [
-  columnHelper.accessor('id', { header: 'ID', enableColumnFilter: false }),
-  columnHelper.accessor('create_at', { header: '등록일' }),
-  columnHelper.accessor('title', { header: '제목', enableSorting: false }),
-  columnHelper.accessor('tick', { header: '클릭수', enableColumnFilter: false }),
-  columnHelper.accessor('start_date', { header: '시작일' }),
-  columnHelper.accessor('end_date', { header: '종료일' }),
-  columnHelper.accessor('page', { header: '페이지', enableSorting: false }),
-  columnHelper.accessor('location', { header: '광고위치' }),
-  columnHelper.accessor('rate', { header: '가중치' }),
-  columnHelper.accessor('image', { header: '이미지', enableSorting: false, enableColumnFilter: false }),
-  columnHelper.accessor('link', {
-    header: '링크',
-    enableSorting: false,
+export const columns: Array<ColumnDef<AD>> = [
+  {
+    accessorKey: 'id',
+    header: 'ID',
     enableColumnFilter: false,
-    cell: ({ getValue, renderValue, row, column, table }) => {
-      const [value, setValue] = useState<string>(getValue());
-      const onBlur = () => {
-        table.options.meta?.updateData(row.index, column.id, value);
-      };
-
-      return (
-        <input
-          className="adsInput"
-          value={value}
-          onChange={e => {
-            renderValue();
-            setValue(e.target.value as string);
-            row.original.link = e.target.value as string;
-          }}
-          onBlur={onBlur}
-        />
-      );
-    },
-  }),
-  columnHelper.accessor('active', { header: '운영', size: 80 }),
-  columnHelper.accessor('timezone', { header: '지역(시)', size: 60 }),
-  columnHelper.accessor('city', { header: '지역(구)', size: 60 }),
-  columnHelper.accessor('edit', {
+    size: 70,
+  },
+  {
+    accessorKey: 'create_at',
+    header: '등록일',
+  },
+  {
+    accessorKey: 'title',
+    header: '제목',
+  },
+  {
+    accessorKey: 'tick',
+    header: '클릭수',
+    enableColumnFilter: false,
+  },
+  {
+    accessorKey: 'start_date',
+    header: '시작일',
+  },
+  {
+    accessorKey: 'end_date',
+    header: '종료일',
+  },
+  {
+    accessorKey: 'page',
+    header: '페이지',
+    size: 70,
+    enableColumnFilter: false,
+  },
+  { accessorKey: 'location', size: 90, header: '광고위치' },
+  { accessorKey: 'rate', header: '가중치' },
+  { accessorKey: 'image', header: '이미지', enableSorting: false, enableColumnFilter: false, size: 200 },
+  { accessorKey: 'link', header: '링크', enableSorting: false, enableColumnFilter: false },
+  { accessorKey: 'active', header: '운영' },
+  { accessorKey: 'timezone', header: '지역(시)', size: 100 },
+  { accessorKey: 'city', header: '지역(구)' },
+  {
+    accessorKey: 'edit',
     header: '수정',
+    size: 70,
     enableColumnFilter: false,
     enableSorting: false,
     cell: ({ getValue, row, column, table }) => {
@@ -70,11 +75,69 @@ export const columns: any = [
             // console.log(row.row.original);
             alert(row.original?.link);
           }}
-          className="bg-main px-5 rounded-md text-white"
+          className="absBtn"
         >
           저장
         </button>
       );
     },
-  }),
+  },
 ];
+
+// export const columns: any = [
+//   columnHelper.accessor('id', { header: 'ID', enableColumnFilter: false }),
+//   columnHelper.accessor('create_at', { header: '등록일' }),
+//   columnHelper.accessor('title', { header: '제목', enableSorting: false }),
+//   columnHelper.accessor('tick', { header: '클릭수', enableColumnFilter: false, }),
+//   columnHelper.accessor('start_date', { header: '시작일' }),
+//   columnHelper.accessor('end_date', { header: '종료일' }),
+//   columnHelper.accessor('page', { header: '페이지', enableSorting: false }),
+//   columnHelper.accessor('location', { header: '광고위치' }),
+//   columnHelper.accessor('rate', { header: '가중치' }),
+//   columnHelper.accessor('image', { header: '이미지', enableSorting: false, enableColumnFilter: false }),
+//   columnHelper.accessor('link', {
+//     header: '링크',
+//     enableSorting: false,
+//     enableColumnFilter: false,
+//     cell: ({ getValue, renderValue, row, column, table }) => {
+//       const [value, setValue] = useState<string>(getValue());
+//       const onBlur = () => {
+//         table.options.meta?.updateData(row.index, column.id, value);
+//       };
+
+//       return (
+//         <input
+//           className="adsInput"
+//           value={value}
+//           onChange={e => {
+//             renderValue();
+//             setValue(e.target.value as string);
+//             row.original.link = e.target.value as string;
+//           }}
+//           onBlur={onBlur}
+//         />
+//       );
+//     },
+//   }),
+//   columnHelper.accessor('active', { header: '운영' }),
+//   columnHelper.accessor('timezone', { header: '지역(시)' }),
+//   columnHelper.accessor('city', { header: '지역(구)' }),
+//   columnHelper.accessor('edit', {
+//     header: '수정',
+//     enableColumnFilter: false,
+//     enableSorting: false,
+//     cell: ({ getValue, row, column, table }) => {
+//       return (
+//         <button
+//           onClick={() => {
+//             // console.log(row.row.original);
+//             alert(row.original?.link);
+//           }}
+//           className="bg-main px-5 rounded-md text-white"
+//         >
+//           저장
+//         </button>
+//       );
+//     },
+//   }),
+// ];
