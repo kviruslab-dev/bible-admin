799219265fea88afad84bed5347ae636be816fc6
Chang Woo Park <whwh3560@gmail.com>
2023-10-17 19:02:15 +0900

    fix: [front] timeout error -< woxios update

--- src/app/(manager)/ads/page.tsx
+++ src/app/(manager)/ads/page.tsx
@@ -1,16 +1,14 @@
 import { GridProvider } from '@/lib/provider/grid-provider';
 import { instance } from '@/utils/woxios';
 import { use } from 'react';
-import { woxios } from 'woxios';
 
 export default function Page({ searchParams }: { searchParams: { type: string } }) {
-  console.log(searchParams);
-  const { data }: any = use(instance.get('/admin/select', { params: { type: searchParams.type }, cache: 'no-cache' }));
+  const data: any = use(instance.get('/admin/select', { params: { type: searchParams.type }, cache: 'no-cache' }));
 
   return (
     <main className="min-h-screen bg-gray-50 flex justify-center">
       <section className="w-[1200px] h-[700px] py-30 px-10 overflow-x-scroll overflow-y-scroll">
-        <GridProvider data={data?.data}></GridProvider>
+        <GridProvider data={data?.data?.data}></GridProvider>
       </section>
     </main>
   );
--- src/constants/column.tsx
+++ src/constants/column.tsx
@@ -22,33 +22,36 @@ interface AD {
 const columnHelper = createColumnHelper();
 
 export const columns: any = [
-  columnHelper.accessor('id', { header: 'ID', size: 30, enableColumnFilter: false }),
-  columnHelper.accessor('create_at', { header: '등록일', size: 200 }),
+  columnHelper.accessor('id', { header: 'ID', enableColumnFilter: false }),
+  columnHelper.accessor('create_at', { header: '등록일' }),
   columnHelper.accessor('title', { header: '제목', enableSorting: false }),
   columnHelper.accessor('tick', { header: '클릭수', enableColumnFilter: false }),
   columnHelper.accessor('start_date', { header: '시작일' }),
   columnHelper.accessor('end_date', { header: '종료일' }),
   columnHelper.accessor('page', { header: '페이지', enableSorting: false }),
-  columnHelper.accessor('location', { header: '광고위치', size: 60 }),
-  columnHelper.accessor('rate', { header: '가중치', size: 80 }),
-  columnHelper.accessor('image', { header: '이미지', size: 120, enableSorting: false, enableColumnFilter: false }),
+  columnHelper.accessor('location', { header: '광고위치' }),
+  columnHelper.accessor('rate', { header: '가중치' }),
+  columnHelper.accessor('image', { header: '이미지', enableSorting: false, enableColumnFilter: false }),
   columnHelper.accessor('link', {
     header: '링크',
-    size: 200,
     enableSorting: false,
     enableColumnFilter: false,
-    cell: ({ getValue, row, column, table }) => {
+    cell: ({ getValue, renderValue, row, column, table }) => {
       const [value, setValue] = useState<string>(getValue());
-      // const linkRef = useRef<HTMLInputElement>(null);
-      // console.log(row);
+      const onBlur = () => {
+        table.options.meta?.updateData(row.index, column.id, value);
+      };
+
       return (
         <input
           className="adsInput"
           value={value}
           onChange={e => {
-            setValue(pre => e.target.value as string);
+            renderValue();
+            setValue(e.target.value as string);
             row.original.link = e.target.value as string;
           }}
+          onBlur={onBlur}
         />
       );
     },
@@ -58,7 +61,6 @@ export const columns: any = [
   columnHelper.accessor('city', { header: '지역(구)', size: 60 }),
   columnHelper.accessor('edit', {
     header: '수정',
-    size: 100,
     enableColumnFilter: false,
     enableSorting: false,
     cell: ({ getValue, row, column, table }) => {
