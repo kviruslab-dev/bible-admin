6934b61542c0675ae3e00d7fe82abd903d9fae2e
Chang Woo Park <whwh3560@gmail.com>
2023-11-17 14:20:11 +0900

    fix: [front] build fail

--- src/app/(form-page)/admin/page.tsx
+++ src/app/(form-page)/admin/page.tsx
@@ -1,7 +1,6 @@
 import { instance } from '@/utils/woxios';
 import { use } from 'react';
 import { Container } from './_container';
-import { ChartBox } from './_component';
 
 export default function App({ searchParams }: any) {
   const { page = '1' } = searchParams;
@@ -16,14 +15,9 @@ export default function App({ searchParams }: any) {
     })
   );
 
-  console.log(data?.data);
-
   return (
     <main className="min-h-screen bg-gray-50 px-50">
       <Container data={data?.data} type={page} />
-      <ChartBox />
     </main>
   );
 }
-
-const status = ['상담신청', '상담취소', '상담완료', '설치완료'];
